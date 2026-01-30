993c3824c089750c22f05766ee3dce7d509d1cbc
anurag8484 <anuragpoonia.ap@gmail.com>
2026-01-30 23:37:34 +0530

    added landing page

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ const Index = () => {
       <Navbar />
       <main>
         <Hero />
-        <QuickInfo />
+        <QuickInfo /> 
         <NewsEvents />
         <WhyColumbia />
         <LearningExperience />
